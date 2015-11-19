f7647adf3f8e7b53086aaf0b4ef0971059d5222e
Jonathon <jonathon.nordquist@gmail.com>
2015-11-19 23:00:58 +0000

    Add new pin link to header.  Add logged_in_user and correct_user methods to pins controller.  Use logged_in_user in before filter.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,4 +1,5 @@
 class PinsController < ApplicationController
+  before_action :logged_in_user, only: [:new, :create, :edit, :destroy]
   def new
     @pin = Pin.new
   end
@@ -21,8 +22,23 @@ class PinsController < ApplicationController
     @pin = Pin.find(params[:id])
   end
   
+  def destroy
+  end
+  
   private
     def pins_params
       params.require(:pin).permit(:title, :description)
+    end
+    
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in"
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(Pin.find_by(params_id).user_id)
+      redirect_to root_url unless @user = current_user
     end
 end
