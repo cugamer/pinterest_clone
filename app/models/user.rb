09b9c0a9c6ee9dab592b17493e357d16c91e326d
Jonathon <jonathon.nordquist@gmail.com>
2015-11-19 16:51:59 +0000

    Add BCrypt gem.  Add password_hash column to migrations.  Add validation and has_secure_password call to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 55 }
+  
   VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL },
                     uniqueness: { case_sensitive: false }
+  
+  validates :password, presence: true, length: { minimum: 6 }                    
+  has_secure_password
 end
