63e70e6d66da05160e29ba6a4a4d073a852af834
Jonathon <jonathon.nordquist@gmail.com>
2015-11-19 15:19:17 +0000

    Create format_title helper to format page titles.

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,10 @@
 module StaticPagesHelper
+  def formatted_title(title = "")
+    title_body = "Pinterest Clone"
+    if !title.empty?
+      "#{title} | #{title_body}"
+    else
+      title_body
+    end
+  end
 end
