3cfba4284174fab5465c10be1d81c04fe18da9fe
Jonathon <jonathon.nordquist@gmail.com>
2015-11-19 16:36:46 +0000

    Correct migration.  Add validations for email and name to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 55 }
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL },
+                    uniqueness: { case_sensitive: false }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,8 @@
 ActiveRecord::Schema.define(version: 20151119161735) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "new"
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
