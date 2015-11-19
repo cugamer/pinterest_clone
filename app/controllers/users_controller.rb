e601876a81c43af3319e5e7768859a6c6be95b36
Jonathon <jonathon.nordquist@gmail.com>
2015-11-19 16:55:55 +0000

    Update user_params method in users controller.  Update form on users/new view to hold password.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      
+      redirect_to root_url
     else
       render "new"
     end
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   
   private
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
   
 end
