2eaafc8c0c5f224dae410ec4cbedd7052a81f2e9
Jonathon <jonathon.nordquist@gmail.com>
2015-11-19 18:42:27 +0000

    Add logged_in? and log_out methods to sessions helper.  Update header view to change links based on session.  Add destroy method to sessions controller.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,4 +6,13 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
+  
+  def logged_in?
+    !current_user.nil?
+  end
+  
+  def logout
+    @current_user = nil
+    session.delete(:user_id)
+  end
 end
