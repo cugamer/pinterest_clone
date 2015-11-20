74d1dec42e735eb3d83ffe2c0a20b7a4f3c9a9eb
Jonathon <jonathon.nordquist@gmail.com>
2015-11-20 16:47:10 +0000

    Add form to edit view.  Add before filters for edit and create methods.  Add create method.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,7 @@
 class PinsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create, :edit, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  
   def new
     @pin = Pin.new
   end
@@ -16,6 +18,10 @@ class PinsController < ApplicationController
   end
 
   def edit
+    @pin = Pin.find(params[:id])
+  end
+  
+  def update
   end
   
   def show
@@ -44,7 +50,7 @@ class PinsController < ApplicationController
     end
     
     def correct_user
-      @user = User.find(Pin.find_by(params_id).user_id)
-      redirect_to root_url unless @user = current_user
+      @user = User.find(Pin.find_by(id: params[:id]).user_id)
+      redirect_to(root_url) unless @user == current_user
     end
 end
