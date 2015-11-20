af3907ac5b4ca8b838d123b18548ace4c6ff9042
Jonathon <jonathon.nordquist@gmail.com>
2015-11-20 22:15:06 +0000

    Updated before filters for pins controller.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,6 @@
 class PinsController < ApplicationController
-  before_action :logged_in_user, only: [:new, :create, :edit, :destroy]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   
   def new
     @pin = Pin.new
