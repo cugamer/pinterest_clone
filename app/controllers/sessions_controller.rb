c7a0c512ec77ae8ec787ba0007dff553fd0e858a
Jonathon <jonathon.nordquist@gmail.com>
2015-11-20 22:17:11 +0000

    Update sessions create method to direct user to pins page on log in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      redirect_to user_pins_page_path(user.id)
     else
       flash.now[:danger] = "Username or password is incorrect."
       render 'new'
