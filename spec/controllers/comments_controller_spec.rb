8449685650f1949892b7ca99f950a41c5c33aca1
Jonathon <jonathon.nordquist@gmail.com>
2015-12-14 22:45:12 +0000

    Add test for comments controller create method.

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+require 'sessions_helper'
+
+RSpec.describe CommentsController, type: :controller do
+  it 'creates a new comment when given valid input' do
+    #  Should stub the pin and user here
+    pin = build(:pin)
+    pin.save
+    params = {:comment => {:pin_id => pin.id,
+                            :comment_text => "comment"}}
+    user = build(:user)
+    user.save
+    
+    session[:user_id] = user.id
+    post(:create, params)
+    expect(response).to render_template('pins/show')
+    expect(response).to have_http_status(200)
+  end
+end
